54df8fc4c38868a4e10c64341cc5a6d8757e45ae
15580344451 <3063670672@qq.com>
2019-09-29 16:39:47 +0800

    自动扫描包，注解添加过滤（根据类名过滤）

--- src/main/java/com/gy/configuration/MyConfiguration.java
+++ src/main/java/com/gy/configuration/MyConfiguration.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.context.annotation.ComponentScan.Filter;
 
 import com.gy.bean.Person;
+import com.gy.dao.BookDao;
 
 @ComponentScan(basePackages="com.gy",excludeFilters= {
-			@Filter(type=FilterType.ANNOTATION,classes= {Controller.class,Service.class})
+			@Filter(type=FilterType.ANNOTATION,classes= {Controller.class,Service.class}),
+			@Filter(type=FilterType.ASSIGNABLE_TYPE,classes=BookDao.class)
 })
 @Configuration
 public class MyConfiguration {
